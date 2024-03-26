01ba8cac9cec359fe98823ac30e1c6b4ea809055
laboydan <goldenknight22@gmail.com>
2024-03-26 19:46:35 -0400

    implement forwarding props

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import CoreConcepts from './components/CoreConcepts.jsx';
-import Examples from './components/Examples.jsx';
+import Examples from './components/Examples/Examples.jsx';
 import Header from './components/Header/Header.jsx';
 
 function App() {
--- React/01-starting-project/src/components/Examples/Examples.jsx
+++ React/01-starting-project/src/components/Examples/Examples.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 
-import TabButton from './TabButton/TabButton.jsx';
+import Section from '../Section.jsx';
+import TabButton from '../TabButton/TabButton.jsx';
 
-import { EXAMPLES } from '../data.js';
+import { EXAMPLES } from '../../data.js';
+import './Examples.css';
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -24,35 +26,34 @@ export default function Examples() {
   }
 
   return (
-    <section id='examples'>
-      <h2>Examples</h2>
+    <Section title='Examples' id='examples'>
       <menu>
         <TabButton
           isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')}
+          onClick={() => handleSelect('components')}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')}
+          onClick={() => handleSelect('jsx')}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')}
+          onClick={() => handleSelect('props')}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')}
+          onClick={() => handleSelect('state')}
         >
           State
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
