fd10f0ebe6b6e370be5d0d6a410aed51a291d094
laboydan <goldenknight22@gmail.com>
2024-03-27 00:04:38 -0400

    refactor gameboard state and implement logs

--- React/07-tic-tac-toe-starting-project/src/App.jsx
+++ React/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react';
 
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 import Player from './components/Player.jsx';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
+    // update active player
     setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    // update game log
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -25,12 +43,9 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
