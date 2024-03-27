1600de9c320c31aba77aa6dbcfdff4b6d8c15345
laboydan <goldenknight22@gmail.com>
2024-03-27 00:41:30 -0400

    refactor game board state, implement winner check

--- React/07-tic-tac-toe-starting-project/src/App.jsx
+++ React/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,24 @@ import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
 import Player from './components/Player.jsx';
 
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+/**
+ * Initial game board state (3x3)
+ */
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+/**
+ * Calculates current active player based on current game log. Player 1 (X)
+ * always goes first.
+ *
+ * @param {Array<String>[]} gameTurns Current game log
+ * @returns Current active player symbol - X or O
+ */
 function deriveActivePlayer(gameTurns) {
   let activePlayer = 'X';
 
@@ -17,8 +35,44 @@ function deriveActivePlayer(gameTurns) {
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
+  // Calculate active player
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  // Calculate current game board
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  // Check for winner
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  /**
+   * Event handler for when clicking a game board square (button).
+   *
+   * @param {number} rowIndex Row of square
+   * @param {number} colIndex Col of square
+   */
   function handleSelectSquare(rowIndex, colIndex) {
     // update game log
     setGameTurns((prevTurns) => {
@@ -48,7 +102,8 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
