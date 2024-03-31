ff396084d3240080a0a88fb159d3ba6f6a705c9d
laboydan <goldenknight22@gmail.com>
2024-03-31 18:39:37 -0400

    implemented tailwind css

--- React/01-styling-project/src/components/AuthInputs.jsx
+++ React/01-styling-project/src/components/AuthInputs.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react';
-import { styled } from 'styled-components';
+import { useState } from "react";
+// import { styled } from 'styled-components';
 
-import Button from './Button.jsx';
-import Input from './Input.jsx';
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
 /**
  * Styled components
  */
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -31,29 +31,32 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id='auth-inputs'>
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="mx-auto w-full max-w-sm rounded bg-gradient-to-b from-stone-700 to-stone-800 p-8 shadow-md"
+    >
+      <div className="mb-6 flex flex-col gap-2">
         <Input
-          type='email'
-          label='Email'
+          type="email"
+          label="Email"
           invalid={emailNotValid}
-          onChange={(event) => handleInputChange('email', event.target.value)}
+          onChange={(event) => handleInputChange("email", event.target.value)}
         />
         <Input
-          type='password'
-          label='Password'
+          type="password"
+          label="Password"
           invalid={passwordNotValid}
           onChange={(event) =>
-            handleInputChange('password', event.target.value)
+            handleInputChange("password", event.target.value)
           }
         />
-      </ControlContainer>
-      <div className='actions'>
-        <button type='button' className='text-button'>
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
--- React/01-styling-project/src/components/Header.jsx
+++ React/01-styling-project/src/components/Header.jsx
@@ -1,56 +1,60 @@
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 // import classes from './Header.module.css';
 
-const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
+// const StyledHeader = styled.header`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   margin-top: 2rem;
+//   margin-bottom: 2rem;
 
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
+//   & img {
+//     object-fit: contain;
+//     margin-bottom: 2rem;
+//     width: 11rem;
+//     height: 11rem;
+//   }
 
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: 'Pacifico', cursive;
-    margin: 0;
-  }
+//   & h1 {
+//     font-size: 1.5rem;
+//     font-weight: 600;
+//     letter-spacing: 0.4em;
+//     text-align: center;
+//     text-transform: uppercase;
+//     color: #9a3412;
+//     font-family: 'Pacifico', cursive;
+//     margin: 0;
+//   }
 
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
+//   & p {
+//     text-align: center;
+//     color: #a39191;
+//     margin: 0;
+//   }
 
-  @media (min-width: 768px) {
-    margin-bottom: 4rem;
+//   @media (min-width: 768px) {
+//     margin-bottom: 4rem;
 
-    & h1 {
-      font-size: 2.25rem;
-    }
-  }
-`;
+//     & h1 {
+//       font-size: 2.25rem;
+//     }
+//   }
+// `;
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt='A canvas' />
-      <h1>ReactArt</h1>
+    // <StyledHeader>
+    <header className="my-8 flex flex-col items-center md:mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 size-44 object-contain" />
+      <h1 className="font-title text-center text-xl font-semibold uppercase tracking-widest text-amber-800 md:text-4xl">
+        ReactArt
+      </h1>
       {/* <p className={classes.paragraph}> */}
-      <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
+    // </StyledHeader>
   );
 }
