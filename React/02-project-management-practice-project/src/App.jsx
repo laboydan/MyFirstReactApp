4f3e7e8750082ced31534c56e055c0108b2c613b
laboydan <goldenknight22@gmail.com>
2024-04-08 19:22:05 -0400

    implement conditional display of new project form

--- React/02-project-management-practice-project/src/App.jsx
+++ React/02-project-management-practice-project/src/App.jsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
+
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let projectContent;
+
+  if (projectsState.selectedProjectId === null) {
+    projectContent = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    projectContent = (
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
+    );
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {projectContent}
     </main>
   );
 }
