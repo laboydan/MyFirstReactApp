7db17623bec12c331b1460c547b4764f3b638a59
laboydan <goldenknight22@gmail.com>
2024-04-08 19:12:18 -0400

    initial layout

--- React/02-project-management-practice-project/src/App.jsx
+++ React/02-project-management-practice-project/src/App.jsx
@@ -1,8 +1,14 @@
+import NewProject from './components/NewProject.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className='h-screen my-8 flex gap-8'>
+      <ProjectsSidebar />
+      <NoProjectSelected />
+      {/* <NewProject /> */}
+    </main>
   );
 }
 
