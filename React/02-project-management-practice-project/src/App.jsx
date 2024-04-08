9fa4f463461fa194167a0810891ce04ca274bfda
laboydan <goldenknight22@gmail.com>
2024-04-08 19:47:46 -0400

    list projects in sidebar

--- React/02-project-management-practice-project/src/App.jsx
+++ React/02-project-management-practice-project/src/App.jsx
@@ -25,12 +25,11 @@ function App() {
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let projectContent;
 
@@ -44,7 +43,10 @@ function App() {
 
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {projectContent}
     </main>
   );
