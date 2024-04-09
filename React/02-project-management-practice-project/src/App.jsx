83884389a0df765ca0c7c86eae26a6e4bbd604fb
laboydan <goldenknight22@gmail.com>
2024-04-08 20:37:03 -0400

    implement project detail page

--- React/02-project-management-practice-project/src/App.jsx
+++ React/02-project-management-practice-project/src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
@@ -40,7 +50,10 @@ function App() {
     });
   }
 
-  let projectContent;
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+  let projectContent = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     projectContent = (
@@ -57,6 +70,7 @@ function App() {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {projectContent}
     </main>
